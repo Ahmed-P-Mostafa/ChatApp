b9883499b0bcb5cc5e5830583ced06b262eb5eb6
Ahmed-P-Mostafa <Ahmed-P-Mostafa@users.noreply.github.com>
2020-12-18 17:53:57 +0200

    Authenticate with google sign in

--- app/src/main/java/com/example/chat/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/chat/ui/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.chat.ui.login
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -14,18 +15,34 @@ import com.example.chat.R
 import com.example.chat.base.BaseActivity
 import com.example.chat.databinding.ActivityLoginBinding
 import com.example.chat.ui.register.RegisterActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.collect
 
 class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(),Navigator {
+    private val TAG = "fireBase LoginActivity"
+    private val RC_SIGN_IN = 9001
+
 
     //val loginViewModel : LoginViewModel by viewModels()
+    lateinit var googleSignInClient: GoogleSignInClient
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         viewModel.navigator = this
-        isUserLogedIn()
+
         dataBinding.vm = viewModel
+        isUserLogedIn()
+
+        googleSignInClient = GoogleSignIn.getClient(this,viewModel.gso)
 
 
         dataBinding.userNameEditText.addTextChangedListener{
@@ -52,7 +69,6 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(),Navig
         return ViewModelProvider(this).get(LoginViewModel::class.java)
     }
     fun isUserLogedIn(){
-
         viewModel.isSignedIn()
         // validate if the user is logged in or not to switch between login or home activity
         viewModel.isSignedLiveData.observe(this, Observer {
@@ -75,10 +91,35 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(),Navig
     }
 
     override fun openRegister() {
+        Log.d(TAG, "openRegister: ")
         val registerIntent = Intent(this,RegisterActivity::class.java)
         startActivity(registerIntent)
 
     }
 
+    override fun openGoogleIntent() {
+        Log.d(TAG, "openGoogleIntent: ")
+
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                Log.d(TAG, "onActivityResult: try")
+                val account :GoogleSignInAccount = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "onActivityResult: fireBaseAuthWithGoogle ${account.id}")
+                Log.d(TAG, "onActivityResult: ")
+                viewModel.firebaseAuthWithGoogle(account.idToken!!)
+
+            }catch (e:ApiException){
+                Log.d(TAG, "onActivityResult: catch error ${e.localizedMessage}")
+                Log.d(TAG, "onActivityResult: catch error $e")
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/example/chat/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/chat/ui/login/LoginViewModel.kt
@@ -1,21 +1,40 @@
 package com.example.chat.ui.login
 
+import android.app.Application
 import android.util.Log
 import android.util.Patterns
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.chat.R
+import com.example.chat.base.BaseActivity
 import com.example.chat.base.BaseViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.channels.consumesAll
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 
 class LoginViewModel:BaseViewModel<Navigator>() {
-    private val TAG = "LoginViewModel"
+    private val TAG = "fireBase LoginViewModel"
 
     val auth = FirebaseAuth.getInstance()
     var isSignedLiveData =  MutableLiveData<Boolean>()
+    private val webClientId = "611522890597-e6dc2r9ld6s6vj014pn36camn5aoil3l.apps.googleusercontent.com"
+    private val webClientSecret = "7hDV_hLKjDHwUXSiQJ59-Gav"
+
+
+    // Configure Google Sign In
+    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(webClientId)
+        .requestEmail()
+        .build()
+
 
 
      var email = MutableStateFlow("")
@@ -25,7 +44,9 @@ class LoginViewModel:BaseViewModel<Navigator>() {
 
 
     fun isSignedIn(){
+
         isSignedLiveData.value = auth.currentUser != null
+        message.postValue(auth.currentUser?.email)
     }
     fun logIn(){
         loader.postValue(true)
@@ -38,7 +59,7 @@ class LoginViewModel:BaseViewModel<Navigator>() {
                 if (it.isSuccessful) {
                     Log.d(TAG, "logIn: login is successful")
                     val user: FirebaseUser? = auth.currentUser
-                    Log.d(TAG, "logIn: ${user?.email}")
+                    Log.d(TAG, "logIn: ${user?.uid}")
                     // update UI
                     message.postValue("login successful")
                 } else {
@@ -49,6 +70,10 @@ class LoginViewModel:BaseViewModel<Navigator>() {
 
             })
 
+    }
+    fun logOut(){
+        auth.signOut()
+        message.postValue(auth.currentUser?.email)
     }
 
     fun setEmail(email:String){
@@ -68,6 +93,33 @@ class LoginViewModel:BaseViewModel<Navigator>() {
         navigator?.openRegister()
 
     }
+    fun loginWithGoogle(){
+        //loader.postValue(true)
+        navigator?.openGoogleIntent()
+
+
+    }
+    fun firebaseAuthWithGoogle(idToken:String){
+        loader.postValue(true)
+        Log.d(TAG, "firebaseAuthWithGoogle: ")
+        val credential = GoogleAuthProvider.getCredential(idToken,null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            loader.postValue(false)
+            Log.d(TAG, "firebaseAuthWithGoogle: authorization completed")
+            if (it.isSuccessful){
+                Log.d(TAG, "firebaseAuthWithGoogle: authorization successful")
+                Log.d(TAG, "firebaseAuthWithGoogle: ${auth.currentUser?.email}")
+                message.postValue("login with google successful")
+            }else{
+                Log.d(TAG, "firebaseAuthWithGoogle: ${it.exception}")
+                message.postValue("login with google failure")
+            }
+
+        }.addOnFailureListener{
+            Log.d(TAG, "firebaseAuthWithGoogle: authorization failed")
+        }
+    }
+
 
     
 }
